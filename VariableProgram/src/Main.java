bd5e0e289e3eec6d3bdaea0265c5d35f149dfa5e
Roberto Jimenez <robertojimenezg@gmail.com>
2023-04-07 14:08:03 -0600

    Tipos de datos, declaracion de variables, asignacion e inicializacion

--- VariableProgram/src/Main.java
+++ VariableProgram/src/Main.java
@@ -1,6 +1,23 @@
 
 public class Main {
 	public static void main(String [] args) {
-		System.out.println("Declaración y uso de variables");
+		System.out.println("Declaración y uso de variables\n");
+		
+		int x;		// Declaration
+		x = 123;	// Assignment
+		
+		// Declaration + Assignment = Initialization
+		// Se pueden usar los dos pasos anteriores en uno solo:
+		
+		int y = 321;
+		double d = 3.1416;
+		boolean z = false;
+		char symbol = '@';
+		
+		System.out.println("La variable x contiene: " + x);
+		System.out.println("Mientras la variable y: " + y);
+		System.out.println("Valiable doble: " + d);
+		System.out.println("Variable booleana: " + z);
+		System.out.println("Variable char: " + symbol);
 	}
 }
