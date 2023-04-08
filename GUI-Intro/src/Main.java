9734c2bf60122626c383e40d18371212c3e121f1
Roberto Jimenez <robertojimenezg@gmail.com>
2023-04-07 20:34:35 -0600

    Uso de showInputDialog y showMessageDialog, además de la conversión de strings a enteros y dobles

--- GUI-Intro/src/Main.java
+++ GUI-Intro/src/Main.java
@@ -1,9 +1,19 @@
 
+import javax.swing.JOptionPane;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		String name = JOptionPane.showInputDialog("Enter your name");
+		JOptionPane.showMessageDialog(null, "Hello " + name);
+		
+		int age = Integer.parseInt(JOptionPane.showInputDialog("Enter your age"));
+		JOptionPane.showMessageDialog(null, "Your age is " + age + " years old");
+		
+		double height = Double.parseDouble(JOptionPane.showInputDialog("Enter your height"));
+		JOptionPane.showMessageDialog(null, "You are " + height + " cm tall");
 	}
 
 }
