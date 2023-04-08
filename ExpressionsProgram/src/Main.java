2188509880774ed33730f84668653af04b1fe4b7
Roberto Jimenez <robertojimenezg@gmail.com>
2023-04-07 19:43:14 -0600

    Expreciones, operadores y operandos en java

---
+++ ExpressionsProgram/src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// Expression = operands & operators
+		// operands = values, variables, numbers, quantity
+		// operators = +. -. *. /, %
+		
+		int friends = 10;
+		
+		friends = friends + 1;
+		System.out.println(friends);
+		
+	}
+
+}
