60fa3392930d9e3dd8075a25598b42f6ea2aeae0
Roberto Jimenez <robertojimenezg@gmail.com>
2023-04-09 20:00:36 -0600

    Random con enteros, flotantes, especificando un llímite y con valores booleanos

--- FuncionesMatemricas/src/RandomProgram.java
+++ FuncionesMatemricas/src/RandomProgram.java
@@ -7,8 +7,18 @@ public class RandomProgram {
 		System.out.println("Función Random");
 		
 		Random randomVar = new Random();
+		
 		int x = randomVar.nextInt();
 		System.out.println(x);
+		
+		// int x = randomVar.nextInt(10); esto nos arroja un numero entre 
+		// cero y nueve. Si deseamos un número entre 1 y 10 simplemente sumamos
+		// 1 a la expresión
+		x = randomVar.nextInt(10) + 1;
+		System.out.println(x);
+		System.out.println("\nTembién se puede usar la función Ramdom con valores Bolleanos");
+		boolean z = randomVar.nextBoolean();
+		System.out.println(z);
 	}
 
 }
