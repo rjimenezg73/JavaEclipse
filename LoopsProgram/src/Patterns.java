6179a41e715212e4cb18b677aab48fe8a6b73ebb
Roberto Jimenez <robertojimenezg@gmail.com>
2023-04-11 13:40:00 -0600

    Half Rectangle with numbers

--- LoopsProgram/src/Patterns.java
+++ LoopsProgram/src/Patterns.java
@@ -70,5 +70,17 @@ public class Patterns {
 			System.out.println();
 		}
 		
+		// Half Rectangle with numbers
+		System.out.println("\nHalf Rectangle with numbers");
+		// outer loop
+		for(int i = 1; i <= m; i++) {
+			// inner loop
+			for(int j = 1; j <= i; j++) {
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+		
+		
 	}
 }
