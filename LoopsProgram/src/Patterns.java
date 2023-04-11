43ac0faa975308d2dd1aaebeccca87a9478b17c8
Roberto Jimenez <robertojimenezg@gmail.com>
2023-04-11 07:58:32 -0600

    Complete rectangle and Hollow rectangle

--- LoopsProgram/src/Patterns.java
+++ LoopsProgram/src/Patterns.java
@@ -7,11 +7,28 @@ public class Patterns {
 		int m = 5;
 		char sim = '^';
 		
+		//Complete Rectangle
+		System.out.println("Complete Rectangle");
 		// outer loop
 		for(int i = 1; i <= n; i++) {
 			// inner loop
-			for(int j = 1; j <= 5; j++) {
+			for(int j = 1; j <= m; j++) {
 				System.out.print(sim);
+			}
+			System.out.println();
+		}
+		
+		// Hollow Rectangle
+		System.out.println("\nHollow Rectangle");
+		// outer loop
+		for(int i = 1; i <= n; i++) {
+			// inner loop
+			for(int j = 1; j <= m; j++) {
+				if(i == 1 || i == n || j == 1 || j == m) {
+					System.out.print(sim);
+				}else {
+					System.out.print(' ');
+				}
 			}
 			System.out.println();
 		}
