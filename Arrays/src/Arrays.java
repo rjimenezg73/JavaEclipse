f20d83031d537d4b064bf7e5e1b91562f4d52e31
rjimenezg73 <robertojimenezg@gmail.com>
2023-04-21 14:39:24 -0600

    Array class 01

---
+++ Arrays/src/Arrays.java
@@ -0,0 +1,14 @@
+
+public class Arrays {
+
+	public static void main(String[] args) {
+		// array = used to store multiple values  in a single variable
+		
+		
+		String[] cars = {"Camaro", "Corvette", "Tesla", "BMW"};
+		cars[0] = "Mustang";
+		
+		System.out.println(cars[3]);
+	}
+
+}
