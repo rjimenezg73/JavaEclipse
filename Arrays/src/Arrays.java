5797419ca71c41aa3e029567b14cdf41a57183df
rjimenezg73 <robertojimenezg@gmail.com>
2023-04-21 21:48:48 -0600

    Arrays clase 02

--- Arrays/src/Arrays.java
+++ Arrays/src/Arrays.java
@@ -6,9 +6,15 @@ public class Arrays {
 		
 		
 		String[] cars = {"Camaro", "Corvette", "Tesla", "BMW"};
-		cars[0] = "Mustang";
+		String[] colors = new String[3];
 		
-		System.out.println(cars[3]);
+		cars[0] = "Mustang";
+		colors[0] = "Red";
+		colors[1] = "Yellow";
+		colors[2] = "Blue";
+		
+		
+		System.out.println(cars[0] + " " + colors[1]);
 	}
 
 }
