c5c707f53dcca07a44d9094d913c9a4aa919bfee
rjimenezg73 <robertojimenezg@gmail.com>
2023-06-03 19:33:32 -0600

    Uso de clases - modificaciones02 ch03 - ingresar saldos flotantes

--- Java_Deitel/src/deitel/java/ch03/Cuenta.java
+++ Java_Deitel/src/deitel/java/ch03/Cuenta.java
@@ -2,10 +2,22 @@ package deitel.java.ch03;
 
 public class Cuenta {
 	private String nombre;
+	private double saldo;
 	
 	// El constructor inicializa nombre con el parámetro nombre
-	public Cuenta(String nombre) {
+	public Cuenta(String nombre, double saldo) {
 		this.nombre = nombre;
+		if(saldo > 0.0)
+			this.saldo = saldo;
+	}
+	
+	public void depositar(double montoDeposito) {
+		if(montoDeposito > 0.0)
+			this.saldo = this.saldo + montoDeposito;
+	}
+	
+	public double obtenerSaldo() {
+		return this.saldo;
 	}
 	
 	public void establecerNombre(String nombre) {
--- Java_Deitel/src/deitel/java/ch03/PruebaCuenta.java
+++ Java_Deitel/src/deitel/java/ch03/PruebaCuenta.java
@@ -30,12 +30,31 @@ public class PruebaCuenta
 		System.out.println("Tipo obj: " + obj);
 		*/
 		
-		Cuenta cuenta1 = new Cuenta("Peter Pica Cu");
-		Cuenta cuenta2 = new Cuenta("Roberto Jimenez");
+		Cuenta cuenta1 = new Cuenta("Peter Pica Cu", 50.00);
+		Cuenta cuenta2 = new Cuenta("Roberto Jimenez", -7.53);
 		
+		// Muestra el saldo inicial de cada cuenta
 		System.out.printf("El nombre de cuenta1 es: %s%n", cuenta1.obtenerNombre());
+		System.out.printf("Saldo de: $%.2f%n", cuenta1.obtenerSaldo());
 		System.out.printf("El nombre de cuenta2 es: %s%n", cuenta2.obtenerNombre());
+		System.out.printf("Saldo de: $%.2f%n", cuenta2.obtenerSaldo());
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.print("Escriba el monto a depositar para cuenta1: $");
+		double montoDeposito = entrada.nextDouble();
+		System.out.printf("%nsumando %.2f al saldo de cuenta1%n%n", montoDeposito);
+		cuenta1.depositar(montoDeposito);
+		System.out.printf("Saldo de %s: $%.2f%n%n", cuenta1.obtenerNombre(), cuenta1.obtenerSaldo());
+		
+		System.out.print("Escriba el monto a depositar para cuenta2: $");
+		montoDeposito = entrada.nextDouble();
+		System.out.printf("%nsumando %.2f al saldo de cuenta2%n%n", montoDeposito);
+		cuenta2.depositar(montoDeposito);
+		System.out.printf("Saldo de %s: $%.2f%n%n", cuenta2.obtenerNombre(), cuenta2.obtenerSaldo());
+		
 
+		
 	}
 
 }
