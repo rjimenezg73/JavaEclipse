174d6ec9ccd70f8f4c5e7ed6c44dfb280bd05a84
rjimenezg73 <robertojimenezg@gmail.com>
2023-06-03 19:42:00 -0600

    Dialog swing

---
+++ Java_Deitel/src/deitel/java/ch03/Dialogo1.java
@@ -0,0 +1,9 @@
+package deitel.java.ch03;
+import javax.swing.JOptionPane;
+
+public class Dialogo1 {
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "Bienvenido a Java");
+	}
+
+}
